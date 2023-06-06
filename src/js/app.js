b46544322639e26309d3d36681370710d0610028
Sergei Korchagin <gamestationbase@gmail.com>
2023-06-06 20:18:49 +0300

    Add starting work

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import Widget from "./widget.js";
+
+const test = new Widget(document.querySelector('.container'));
+test.createModule();
+
+const button = document.querySelector('.btn-open');
+const contentContainer = document.querySelector('.content-container');
+
+button.addEventListener('click', (ev) => {
+ev.preventDefault();
+
+contentContainer.classList.toggle('expansion-hide');
+
+});
